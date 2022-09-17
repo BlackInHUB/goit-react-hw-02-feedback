3d98382f4264ba3d2bba65cef84986f0bad06689
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-09-17 12:05:08 +0300

    Feedback done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,34 @@
 import React from "react";
-import {Feedback} from './Feedback/Feedback'
+import { Section } from "./Feedback/Section/Section";
+import { FeedbackOptions } from './Feedback/FeedbackOptions/FeedbackOptions'
+import { Statistics } from './Feedback/Statistics/Statistics'
+import { Notification } from './Feedback/Notification/Notification'
 
 class App extends React.Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
   }
 
   handleBtnClick = (event) => {
-    const tartgetBtnName = event.target.name;
-
+    const tartgetBtn = event.target.name;
+    
     this.setState(prevState => ({
-      [tartgetBtnName]: prevState[tartgetBtnName] + 1
+      [tartgetBtn]: prevState[tartgetBtn] + 1
     }))
   }
 
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positivePercentage = Number((((good + neutral) / total) * 100).toFixed());
+
     return (
-      <Feedback values={this.state} onClick={this.handleBtnClick} />
+      <Section title="Please, leave your feedback">
+        <FeedbackOptions options={this.state} onLeaveFeedback={this.handleBtnClick} />
+        {total > 0 ? <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/> : <Notification />}
+      </Section>
     )
   }
 };
